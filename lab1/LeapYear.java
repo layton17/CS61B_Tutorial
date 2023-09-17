f9ecbe02cee4f9e95b68b52f214972904bd6c867
layton17 <layton17@foxmail.com>
2023-09-17 12:42:30 +0800

    第一次提交

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,10 +2,9 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
-
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
-     */
+      public static boolean isLeapYear(int year){
+       return ((year%4==0&&year%100!=0)||year%400==0);//判断闰年
+  }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
